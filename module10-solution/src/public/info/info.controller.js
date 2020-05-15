e0e60f2f8c0ca5eb7a7004ea14dafbec1242f4f8
Brady, Kelly A <kelly.brady@jhuapl.edu>
2020-05-14 22:18:57 -0400

    yay

--- module10-solution/src/public/info/info.controller.js
+++ module10-solution/src/public/info/info.controller.js
@@ -4,14 +4,16 @@
     angular.module('public')
     .controller('InfoController', InfoController);
     
-    InfoController.$inject = ["PublicService"];
-    function InfoController(PublicService) {
+    InfoController.$inject = ["PublicService", 'ApiPath'];
+    function InfoController(PublicService, ApiPath) {
       var sctrl = this;
       sctrl.user_data = PublicService.get_user_data();
       sctrl.firstname = sctrl.user_data[0];
       sctrl.lastname = sctrl.user_data[1];
       sctrl.phone = sctrl.user_data[2];
       sctrl.email = sctrl.user_data[3];
+
+      sctrl.basePath = ApiPath;
 
 
     }
